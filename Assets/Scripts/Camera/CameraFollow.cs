22174f7d8a172ab1b28c39194315a2a55abf0a28
escruz <emmanuel.salva.cruz@gmail.com>
2018-04-12 07:03:58 -0700

    Add test scene for player movement
    - Add a simple title screen
    - Add a test scene
    - Add camera targetting
    - Add player movement and jumping controls

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Robo {
+
+    // A very simple camera follow. Doesn't offer a great ton of smoothing since its run on FixedUpdate.
+    // Will fix this to Update following a dummy transform when I have more time left.
+    public class CameraFollow : MonoBehaviour {
+
+        public Transform target;
+        public float smoothing = 10f;
+        public Vector3 offset;
+        
+        private void FixedUpdate() {
+            
+            transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothing * Time.deltaTime);
+            
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Robo {
+
+    public class PlayerController : MonoBehaviour {
+        
+        public float speedSmoothing = 10f;
+        public float speed = 6f;
+        public float jumpSpeed = 4f;
+        public float groundCheckDistance = 0.5f;
+
+        public Transform[] groundChecks;
+
+        Rigidbody m_RigidBody;
+        Vector3 m_MoveDirection;
+
+        float horizontal;
+        bool inputJump;
+        bool jumpPending;
+
+        private void Awake() {
+            m_RigidBody = GetComponent<Rigidbody>();
+        }
+        
+        private void Update() {
+            horizontal = Input.GetAxis("Horizontal");
+            inputJump = Input.GetButtonDown("Jump");
+            if (inputJump && !jumpPending) {
+                jumpPending = true;
+            } 
+        }
+
+        private void FixedUpdate() {
+            Move();
+            Jump();
+        }
+
+        private void Move() {
+            m_MoveDirection = new Vector3(horizontal, 0, 0);
+            m_MoveDirection = m_MoveDirection * speed * Time.deltaTime;
+            m_RigidBody.MovePosition(transform.position + m_MoveDirection);
+        }
+
+        private void Jump() {
+            if (jumpPending && IsGrounded()) {
+                //m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, m_RigidBody.velocity.y * jumpSpeed, m_RigidBody.velocity.z);
+                m_RigidBody.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+            }
+            jumpPending = false;
+        }
+
+        private bool IsGrounded() {
+            foreach (var groundCheck in groundChecks) {
+                Debug.DrawRay(groundCheck.position, Vector3.down * groundCheckDistance, Color.red);
+                RaycastHit hit;
+                if (Physics.Raycast(groundCheck.position, Vector3.down, out hit, groundCheckDistance)) {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+    }
+
+}
