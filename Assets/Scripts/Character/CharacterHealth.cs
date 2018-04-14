d370f9101a76cc68d6dbf332a20a7f21fc997670
escruz <emmanuel.salva.cruz@gmail.com>
2018-04-14 04:20:52 -0700

    Add attack and double jump for player control
    - add double jump ability
    - add attack animation
    - add free effects resources
    - remove prototype cubes

---
+++ Assets/Scripts/Character/CharacterHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Robo {
+
+    public class CharacterHealth : MonoBehaviour {
+
+        public int maxHp;
+        public int hp;
+        
+    }
+
+}
--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -7,20 +7,23 @@ namespace Robo {
     // Controling movement and attacks for the player
     public class PlayerController : MonoBehaviour {
         
-        [Header("Speed")]
+        [Header("Move Speed")]
         public float speed = 6f;
         public float speedSmoothing = 10f;
         public float turnSpeed = 10f;
-        public float jumpSpeed = 8f;
+        public float jumpSpeed = 16f;
+        public float doubleJumpSpeed = 12f;
 
         [Header("Ground Checks")]
         [Tooltip("Points to use for raycasting to the ground")]
         public Transform[] groundChecks;
         public float groundCheckDistance = 0.5f;
 
-        // state flags
-        bool isFacingRight = true;
-
+        [Header("Skills")]
+        public float attackDuration = 1f;
+        public float attackForce = 2f;
+        public bool canDoubleJump = false;        
+        
         float deadZone = 0.01f;
 
         Rigidbody m_RigidBody;
@@ -32,6 +35,10 @@ namespace Robo {
         float horizontal;
         bool jumpPending;
         bool attackPending;
+
+        bool isFacingRight = true;
+        float timerAttack = 0;
+        bool usedDoubleJump = false;
 
         //--------------------------------------------
         // Unity Methods
@@ -50,7 +57,14 @@ namespace Robo {
             Move();
             Jump();
             Attack();
-            Animate();
+            AnimateMovement();
+        }
+
+        //--------------------------------------------
+        // Flags
+        //--------------------------------------------
+        bool IsAttacking() {
+            return timerAttack != 0;
         }
 
         //--------------------------------------------
@@ -69,7 +83,14 @@ namespace Robo {
             }
         }
 
+        //--------------------------------------------
+        // Movement
+        //--------------------------------------------
+
         private void Move() {
+
+            if (IsAttacking()) return;
+
             // move the player
             m_MoveDirection = new Vector3(horizontal, 0, 0);
             m_MoveDirection = m_MoveDirection * speed * Time.deltaTime;
@@ -96,12 +117,32 @@ namespace Robo {
 
         private void Jump() {
 
-            if (jumpPending && IsGrounded()) {
-                m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0, m_RigidBody.velocity.z);
-                m_RigidBody.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
+            if (IsAttacking()) {
+                // if attacking eat the input
+                jumpPending = false;
+                return;
             }
-            
+
+            if (jumpPending) {
+
+                if (IsGrounded()) {
+                    usedDoubleJump = false;
+                    DoJump(jumpSpeed);
+                } else if (canDoubleJump && !usedDoubleJump) {
+                    usedDoubleJump = true;
+                    DoJump(doubleJumpSpeed);
+                }
+                
+            }
+
             jumpPending = false;
+
+        }
+
+        private void DoJump(float jSpeed) {
+            m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0, m_RigidBody.velocity.z);
+            m_RigidBody.angularVelocity = Vector3.zero;
+            m_RigidBody.AddForce(new Vector3(0, jSpeed, 0), ForceMode.Impulse);
         }
         
         // IsGrounded - used for jumping
@@ -117,15 +158,7 @@ namespace Robo {
             return false;
         }
 
-        private void Attack() {
-            // TODO
-            if (attackPending) {
-                m_Animator.SetTrigger("Attack");
-            }
-            attackPending = false;
-        }
-        
-        private void Animate() {
+        private void AnimateMovement() {
 
             // IsGrounded() is expensive because of Raycast. 
             // Will have to just check velocity to handle animation
@@ -140,6 +173,40 @@ namespace Robo {
             m_Animator.SetBool("IsJumping", m_RigidBody.velocity.y > deadZone);
             m_Animator.SetBool("IsFalling", m_RigidBody.velocity.y < deadZone);
 
+        }
+
+        //--------------------------------------------
+        // Attack
+        //--------------------------------------------
+        private void Attack() {
+            
+            if (timerAttack > 0) {
+                timerAttack -= Time.deltaTime;
+                if (timerAttack <= 0) {
+                    m_RigidBody.velocity = Vector3.zero;
+                    m_RigidBody.angularVelocity = Vector3.zero;
+                    m_RigidBody.useGravity = true;
+                }
+            } else {
+                timerAttack = 0;
+            }
+
+            if (attackPending && !IsAttacking()) {
+                if (IsGrounded()) {
+                    DoAttack();
+                }
+            }
+
+            attackPending = false;
+        }
+
+        private void DoAttack() {
+            timerAttack = attackDuration;
+            m_RigidBody.velocity = Vector3.zero;
+            m_RigidBody.angularVelocity = Vector3.zero;
+            m_RigidBody.AddForce(transform.forward * attackForce, ForceMode.Impulse);
+            m_Animator.SetTrigger("Attack");
+            m_RigidBody.useGravity = false;
         }
 
     }
